bb1b697f753b40c58675c4fd0299cc7076804757
Ben Eich <jgradio64@yahoo.com>
2024-01-13 23:27:44 -0500

    update setup script and gitignore

--- setup.sh
+++ setup.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
+# Install only works on debian based systems
+apt install python3.10-venv
+
+# Create a vitrual environment
+python3.10 -m venv env
+
+# Activate the virtual environment
+source env/bin/activate
 
 # Install package requirements
-echo 'Installing requirements' 
 pip install -r requirements.txt
